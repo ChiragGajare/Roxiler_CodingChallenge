09bbdadf66037374ac91b5d5368c4cfb51b794bf
chirag <chiraggajare72@gmail.com>
2024-11-09 12:22:53 +0530

    3rd commit

--- Frontend_Task/my-react-app/src/App.jsx
+++ Frontend_Task/my-react-app/src/App.jsx
@@ -3,70 +3,57 @@ import "./App.css";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 
+
+
 function App() {
-  // const [trans, setTrans] = useState([]);
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:8080/transactions")
-      // .then((trans) => setTrans(trans.data))
-      .then((response) => {setData(response.data)})
-      //.then(trans=>console.log(trans.data))
+      .then((response) => {
+        setData(response.data);
+      })
+      //.then(response=>console.log(response.data))
       .catch((err) => console.log(err));
   }, []);
 
   const columns = [
     {
+      name: "Id",
+      selector: (row) => row.id,
+      sortable: true,
+      width: '2%',
+    },
+    {
       name: "Title",
       selector: (row) => row.title,
+      sortable: true,
+      width: '15%',
     },
     {
       name: "Description",
       selector: (row) => row.description,
+      sortable: true,
+      width: '18%',
+
+
     },
     {
       name: "Price",
       selector: (row) => row.price,
+      sortable: true,
+      grow: 'false',
+      width: '15%',
     },
   ];
 
   return (
-    <>
-      {/* <div className='container'>
-        <h1>React app</h1>
-        <table className='table'border={1}>
-          <thead>
-            <tr>
-              <th>
-                Title
-              </th>
-              <th>
-                Description
-              </th>
-              <th>
-                Price
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {trans.map(trans=>{
-              return(
-              <tr>
-                <td>{trans.title}</td>
-                <td>{trans.description}</td>
-                <td>{trans.price}</td>
-              </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div> */}
-
-      <DataTable columns={columns} data={data} pagination>
-          
-      </DataTable>
-    </>
+    <div className="container">
+      <div className="container mt-5 border">
+        <DataTable columns={columns} showGridlines data={data} pagination ></DataTable>
+      </div>
+    </div>
   );
 }
 
